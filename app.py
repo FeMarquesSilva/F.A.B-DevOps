0495fda1ccbed22d98c59e7e9fc873992db6f8f3
FeMarquesSilva <felipemarques330@gmail.com>
2024-11-01 20:45:38 -0300

    Adicionar funcionalidade de cadastro de aluno

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def listar_alunos():
     return jsonify(output)
 
 # Rota para adicionar um aluno - Método POST
-@app.route('/aluno', methods=['POST'])
+@app.route('/aluno', methods=['GET'])
 def adicionar_aluno():
     data = request.get_json()
 
